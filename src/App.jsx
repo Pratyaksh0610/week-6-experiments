58715c3d22c0cea7703d39eb7bacb4395b55fb12
Pratyaksh <pratyaksh0610@gmail.com>
2024-02-21 03:49:32 +0530

    Card Wrapper

--- src/App.jsx
+++ src/App.jsx
@@ -6,58 +6,67 @@ function App() {
   // let i = 0;
   return (
     <>
-      <TaskAdder todo={todo} setTodo={setTodo}></TaskAdder>
+      <CardWrapper innerComponent={<TextComponent />} />
+      {/* <TaskAdder todo={todo} setTodo={setTodo}></TaskAdder>
       {console.log(todo.length)}
-      <TaskDisplayer todo={todo}></TaskDisplayer>
+      <TaskDisplayer todo={todo}></TaskDisplayer> */}
     </>
   );
 }
 
-function TaskDisplayer(props) {
-  const arr = props.todo;
-  return (
-    <div>
-      {arr.map((task) => {
-        return (
-          <div>
-            <h1>{task.title}</h1>
-            <p>{task.description}</p>
-          </div>
-        );
-      })}
-    </div>
-  );
+function TextComponent() {
+  return <h1>hi there</h1>;
 }
 
-function TaskAdder(props) {
-  const [taskName, setTaskName] = useState("");
-  const [taskDescription, setTaskDescription] = useState("");
-  return (
-    <>
-      <input
-        placeholder="Name"
-        onChange={(e) => setTaskName(e.target.value)}
-      ></input>
-      <input
-        placeholder="Description"
-        onChange={(e) => setTaskDescription(e.target.value)}
-      ></input>
-      <button
-        onClick={() => {
-          const newTask = {
-            title: taskName,
-            description: taskDescription,
-          };
-          const arr = props.todo;
-          const prevTasks = [...arr];
-          prevTasks.push(newTask);
-          props.setTodo(prevTasks);
-        }}
-      >
-        Add task
-      </button>
-    </>
-  );
+function CardWrapper({ innerComponent }) {
+  return <div style={{ border: "2px solid black" }}>{innerComponent}</div>;
 }
+
+// // function TaskDisplayer(props) {
+// const arr = props.todo;
+// return (
+//   <div>
+//     {arr.map((task) => {
+//       return (
+//         <div>
+//           <h1>{task.title}</h1>
+//           <p>{task.description}</p>
+//         </div>
+//       );
+//     })}
+//   </div>
+// );
+// // }
+
+// function TaskAdder(props) {
+//   const [taskName, setTaskName] = useState("");
+//   const [taskDescription, setTaskDescription] = useState("");
+//   return (
+//     <>
+//       <input
+//         placeholder="Name"
+//         onChange={(e) => setTaskName(e.target.value)}
+//       ></input>
+//       <input
+//         placeholder="Description"
+//         onChange={(e) => setTaskDescription(e.target.value)}
+//       ></input>
+//       <button
+//         onClick={() => {
+//           const newTask = {
+//             title: taskName,
+//             description: taskDescription,
+//           };
+//           const arr = props.todo;
+//           const prevTasks = [...arr];
+//           prevTasks.push(newTask);
+//           props.setTodo(prevTasks);
+//         }}
+//       >
+//         Add task
+//       </button>
+//     </>
+//   );
+// }
 
 export default App;
